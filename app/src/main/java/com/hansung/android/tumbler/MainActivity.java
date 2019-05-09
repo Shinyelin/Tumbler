0e8a7ac03427e66685294feaeadcc77351efb069
Shinyelin <50151242+Shinyelin@users.noreply.github.com>
2019-05-09 17:34:44 +0900

    fragment
    fragment 추가

--- app/src/main/java/com/hansung/android/tumbler/MainActivity.java
+++ app/src/main/java/com/hansung/android/tumbler/MainActivity.java
@@ -2,6 +2,8 @@ package com.hansung.android.tumbler;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
@@ -9,23 +11,29 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
-
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    // 4개의 메뉴에 들어갈 Fragment들
+    private Menu1Fragment menu1Fragment = new Menu1Fragment();
+    private Menu2Fragment menu2Fragment = new Menu2Fragment();
+    private Menu3Fragment menu3Fragment = new Menu3Fragment();
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-
-                    return true;
+                case R.id.navigation_home: {
+                    transaction.replace(R.id.frame_layout, menu1Fragment).commitAllowingStateLoss();
+                    break;
+                }
+                case R.id.navigation_dashboard:{
+                    transaction.replace(R.id.frame_layout, menu2Fragment).commitAllowingStateLoss();
+                    break;
+                }
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    transaction.replace(R.id.frame_layout, menu3Fragment).commitAllowingStateLoss();
+                    break;
             }
             return false;
         }
---
+++ app/src/main/java/com/hansung/android/tumbler/Menu1Fragment.java
@@ -0,0 +1,23 @@
+package com.hansung.android.tumbler;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Menu1Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_menu1, container, false);
+    }
+}
